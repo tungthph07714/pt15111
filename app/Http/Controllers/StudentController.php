8bfdc96c65ca6f97c22c139f3589e72fde8fada5
tungthph07714 <tungthph07714@fpt.edu.vn>
2020-10-07 17:59:32 +0700

    newupdate

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use DB;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // index() su dung phuong thuc GET, route name la students.index
+    public function index()
+    {
+        $students = Student::all(); //lay ra het
+        // hoac $students = Student::orderBy('id', 'desc')->get();
+
+        return view('students.list', ['students' => $students]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // create() su dung phuong thuc GET, route name la students.create
+    public function create()
+    {
+        dd('Student Controller create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = new Student;
+
+        $student->name = $request->name;
+        // ....
+        $student->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    // show su dung phuong thuc GET, route name la students.show
+    public function show(Student $student)
+    {
+        // // Neu chi truyen vao $student -> nhan duoc id cua Student
+        // $studentObjEloquentModel = Student::find($student);
+        // $studentObjQueryBuilder = DB::table('students')->find($student);
+        // dd($studentObjEloquentModel->name, $studentObjQueryBuilder->name);
+
+        // // Neu truyen Student $student -> thuc hien truy van tim Student co id = $student
+        // $studentObj = $student;
+
+        return view('students.show', ['student' => $student]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        return view('students.edit', ['student' => $student]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        // dd($request->all());
+        // Gan gia tri moi cho cac thuoc tinh cua student can update
+        $student->name = $request->name;
+
+        // Thuc hien goi phuong thuc save() de luu du lieu
+        $student->save();
+
+        // Cach 2: $student->update(['name' => $request->name]);
+        // Hoac $student->update([$request->all()])
+        // Khong can save
+
+        return redirect()->route('students.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        // Kiem tra ton tai sinh vien -> xoa
+        if($student) {
+            $student->delete(); // tra ve ket qua true/false
+        }
+
+        // Cach 2: Student::destroy($student->id); // tra ve so luong ban ghi bi xoa
+        // Redirect ve danh sach (co thuc hien truy van lay ds moi)
+        return redirect()->route('students.index');
+    }
+}
---
+++ resources/views/students/list.blade.php
@@ -0,0 +1,56 @@
+<!-- Ke thua view master: admin-layout/master.blade.php -->
+@extends('admin-layout.master')
+
+<!-- Thay doi noi dung don gian -->
+@section('title', 'Title list extends from admin-layout')
+
+@section('content-header', 'Danh sach sinh vien')
+<!-- Thay doi noi dung phuc tap -->
+@section('content')
+  <table border='1' class='table'>
+      <thead>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Age</th>
+          <th>Gender</th>
+          <th>Address</th>
+          <th>Active</th>
+          <th>Actions</th>
+      </thead>
+      <tbody>
+          @foreach($students as $student)
+              <tr>
+                  <td>{{ $student->name }}</td>
+                  <td>{{ $student->phone }}</td>
+                  <td>{{ $student->age }} </td>
+                  <td>
+                      @if ($student->gender == 0)
+                          Female
+                      @elseif ($student->gender == 1)
+                          Male
+                      @else
+                          Nothing
+                      @endif
+                  </td>
+                  <td>{{ $student->address }}</td>
+                  <td>{{ $student->is_active == 1 ? 'Yes' : 'No' }}</td>
+                  <td>
+                    <form
+                      action="{{ route('students.destroy', $student->id) }}"
+                      method="POST"
+                    >
+                      @csrf
+                      <input type='hidden' name='_method' value='DELETE'></input>
+                      <button type='submit'>Delete</button>
+                    </form>
+                    <a href="{{ route('students.edit', $student->id) }}">
+                      <button>Edit</button>
+                    </a>
+                  </td>
+              </tr>
+          @endforeach
+      </tbody>
+  </table>
+@endsection
+
+@section('footer', 'Footer list extends')
